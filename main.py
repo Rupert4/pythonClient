65ace6abba2c0248e990bc94f7c3d98bf42fe32b
Rupert4 <110915510+Rupert4@users.noreply.github.com>
2023-12-22 18:38:14 +0300

    form open

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import sys
+import datetime
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainWindows(QtWidgets.QMainWindow):
+    ServerAdress = "http://locallhost:5000"
+    MessageID = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindows, self).__init__(*args, **kwargs)
+        uic.loadUi('Messanger.ui', self)
+        # self.SendButton.clicked.connect(self.SendButton_clicked)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindows()
+    window.show()
+    sys.exit(app.exec())
