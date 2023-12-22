78d6b947274a9117efaf878095e4e24cd5e39d23
Rupert4 <110915510+Rupert4@users.noreply.github.com>
2023-12-22 21:31:48 +0300

    add send

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 import datetime
+import json
 from PyQt5 import uic, QtCore, QtGui, QtWidgets
+import requests
+from requests.exceptions import HTTPError
 
 
 class MainWindows(QtWidgets.QMainWindow):
@@ -10,7 +13,20 @@ class MainWindows(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindows, self).__init__(*args, **kwargs)
         uic.loadUi('Messanger.ui', self)
-        # self.SendButton.clicked.connect(self.SendButton_clicked)
+        self.SendButton.clicked.connect(self.SendButton_clicked)
+
+    def SendButton_clicked(self):
+        self.SendMessage()
+    def SendMessage(self):
+        UserName = self.UserName.text()
+        MessageText = self.Message.text()
+        TimeStamp = str(datetime.datetime.today())
+        message = f"{{\"UserName\": \"{UserName}\",\"MessageText\": \"{MessageText}\", \"TimeStamp\": \"{TimeStamp}\"}}"
+        print("sended message:" + message)
+        url = self.ServerAdress + "/api/Messanger"
+        data = json.loads(message)
+        request = requests.post(url,json =data)
+        #print(request.status_code, request.reason)
 
 
 # Press the green button in the gutter to run the script.
